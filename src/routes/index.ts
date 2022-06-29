f39e6566efc6728147b288c4390c0121fb2cbd91
ラクーン <58646028+NotARacc@users.noreply.github.com>
2022-06-29 10:57:00 +0000

    Setup to serve photos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,6 @@
-import { Router } from 'express';
+import { Router} from 'express';
 import { apiRoute } from './api';
 
 export const defaultRoute = Router();
 
-defaultRoute.get('/', (_req, res) => {
-    res.redirect("/api/status");
-});
 defaultRoute.use('/api', apiRoute);
--- src/utils/routeFunctions.ts
+++ src/utils/routeFunctions.ts
@@ -12,3 +12,16 @@ export async function canUpload(
   if (!user) return res.status(401).send("Invalid token");
   next();
 }
+
+export async function checkPhoto(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log("running");
+  if (req.url.match(new RegExp("^.+\\."))) {
+    return res
+      .status(200)
+      .sendFile(req.url.substring(1), { root: "./storage" });
+  } else next();
+}
