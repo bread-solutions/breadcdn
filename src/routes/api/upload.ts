b1db31d196568cafba24db4daabf397607aaf1c0
ラクーン <58646028+NotARacc@users.noreply.github.com>
2022-06-29 08:25:59 +0000

    Add upload route

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -1,10 +1,17 @@
 import { Router, Request, Response } from "express";
-
+import { canUpload } from "../../utils/routeFunctions";
+import { formidable } from 'formidable';
 export const uploadRoute = Router();
 
-uploadRoute.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.send({
-    status: "ok",
+uploadRoute.post("/", canUpload, (req: Request, res: Response) => {
+  formidable({uploadDir: 'storage', keepExtensions: true, allowEmptyFiles: false, }).parse(req, (err, fields, files) => {
+    if(err) {
+      res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        res.end(String(err));
+        return;
+    }
+    console.log(files.file);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ fields, files }, null, 2));
   });
 });
--- src/utils/routeFunctions.ts
+++ src/utils/routeFunctions.ts
@@ -1,3 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { getUser } from "../db";
+
 export async function canUpload(req: Request, res: Response, next: NextFunction) {
-    console.log("canUpload");
+    const uploadToken = req.query["token"] as string;
+    if(!uploadToken) return res.status(401).send("No token provided");
+    const user = await getUser({authToken: uploadToken}).catch(() => null);
+    if(!user) return res.status(401).send("Invalid token");
+    next();
 }
