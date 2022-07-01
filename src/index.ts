901754e748c9f994497e28db26bca22b4f04a026
Racc <kyler.g777@gmail.com>
2022-07-01 00:07:12 -0700

    Add redirect for discord invite

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ expressApp.use(express.static("storage"));
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }));
 expressApp.use("/", defaultRoute);
+expressApp.all("/", (_req, res) => {return res.redirect("https://discord.gg/JckpxefJzu")});
 expressApp.listen(18133, () => {
   console.log("Server started on port 18133");
 });
