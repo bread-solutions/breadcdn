df5b397b639885aa11275fd743ed65c662676ccd
Racc <kyler.g777@gmail.com>
2022-07-03 18:52:52 -0700

    Default api route to status

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,7 +4,7 @@ import { statusRoute } from "./status";
 import { uploadRoute } from "./upload";
 
 export const apiRoute = Router();
-
+apiRoute.get('/', statusRoute);
 apiRoute.use("/status", statusRoute);
 apiRoute.use("/upload", uploadRoute);
 apiRoute.use("/delete", deleteRoute);
