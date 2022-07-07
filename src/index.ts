808eebcf06030819c2952c64db57c2009321fdb5
Racc <kyler.g777@gmail.com>
2022-07-07 05:27:44 -0700

    Start EJS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { join } from "path";
+import config from "./config";
 import { defaultRoute } from "./routes";
 const expressApp = express();
+expressApp.use('/static', express.static(join(__dirname, "./static")));
 expressApp.use(express.static(join(__dirname, "../storage")));
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }));
+expressApp.set("views", join(__dirname, "views"));
+expressApp.set('view engine', 'ejs');
 expressApp.use("/", defaultRoute);
-expressApp.all("/", (_req, res) => {return res.redirect("https://discord.gg/JckpxefJzu")});
-expressApp.listen(18133, () => {
-  console.log("Server started on port 18133");
+expressApp.get("/", (_req: Request, res: Response) => {res.render("index")});
+expressApp.all("/discord", (_req, res) => {return res.redirect("https://discord.gg/JckpxefJzu")});
+expressApp.listen(config.port, () => {
+  console.log("Server started on port " + config.port);
 });
